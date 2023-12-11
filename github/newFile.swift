c87b01363640669435b40e94dd15a18c14416a1b
iLujainAbdullah <lujain.abh@gmail.com>
2023-12-11 10:48:38 +0300

    c

---
+++ github/newFile.swift
@@ -0,0 +1,18 @@
+//
+//  newFile.swift
+//  github
+//
+//  Created by Lujain Abdullah Halabi Almeri on 27/05/1445 AH.
+//
+
+import SwiftUI
+
+struct newFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newFile()
+}
