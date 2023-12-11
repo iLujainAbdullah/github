66b5e8eaa900e006b3c8e1c4fe7fc24582eaec42
iLujainAbdullah <lujain.abh@gmail.com>
2023-12-11 10:31:09 +0300

    Initial Commit

---
+++ github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github
+//
+//  Created by Lujain Abdullah Halabi Almeri on 27/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Lujain Abdullah Halabi Almeri on 27/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
